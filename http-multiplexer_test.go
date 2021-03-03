f9f6973f0fdbc5130df3ded776734d10abc0eedc
Aleksandr Tupikov <2pikov@gmail.com>
2021-03-03 22:32:08 +0300

    Refactoring

--- http-multiplexer_test.go
+++ http-multiplexer_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestS(t *testing.T) {
+func TestHandler(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, r.RequestURI)
 	}))
